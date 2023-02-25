c0ed9efe3d94f6801569707685455c8cf2595fdc
Ernest Teluk <teluk.ernest@gmail.com>
2023-02-25 18:02:21 +0100

    clone components from unstyled and fix linting

---
+++ src/index.ts
@@ -0,0 +1,56 @@
+export { default as AutoField, AutoFieldProps } from './components/AutoField';
+export {
+  default as AutoFields,
+  AutoFieldsProps,
+} from './components/AutoFields';
+export { default as AutoForm } from './components/AutoForm';
+export { default as BaseForm } from './components/BaseForm';
+export { default as BoolField, BoolFieldProps } from './components/BoolField';
+export { default as DateField, DateFieldProps } from './components/DateField';
+export {
+  default as ErrorField,
+  ErrorFieldProps,
+} from './components/ErrorField';
+export {
+  default as ErrorsField,
+  ErrorsFieldProps,
+} from './components/ErrorsField';
+export {
+  default as HiddenField,
+  HiddenFieldProps,
+} from './components/HiddenField';
+export {
+  default as ListAddField,
+  ListAddFieldProps,
+} from './components/ListAddField';
+export {
+  default as ListDelField,
+  ListDelFieldProps,
+} from './components/ListDelField';
+export { default as ListField, ListFieldProps } from './components/ListField';
+export {
+  default as ListItemField,
+  ListItemFieldProps,
+} from './components/ListItemField';
+export {
+  default as LongTextField,
+  LongTextFieldProps,
+} from './components/LongTextField';
+export { default as NestField, NestFieldProps } from './components/NestField';
+export { default as NumField, NumFieldProps } from './components/NumField';
+export { default as QuickForm } from './components/QuickForm';
+export {
+  default as RadioField,
+  RadioFieldProps,
+} from './components/RadioField';
+export {
+  default as SelectField,
+  SelectFieldProps,
+} from './components/SelectField';
+export {
+  default as SubmitField,
+  SubmitFieldProps,
+} from './components/SubmitField';
+export { default as TextField, TextFieldProps } from './components/TextField';
+export { default as ValidatedForm } from './components/ValidatedForm';
+export { default as ValidatedQuickForm } from './components/ValidatedQuickForm';
